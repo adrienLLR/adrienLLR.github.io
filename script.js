91a6e327786978603c6d890d43f6d4ed01fe0a13
adrien <adrien.le-louer@grenoble-inp.org>
2023-03-30 18:02:49 +0200

    Add buttons for changing between projects

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const themeToggle = document.getElementById('theme-toggle');
+const aiProjectsBtn = document.getElementById('ai-projects-btn');
+const softwareProjectsBtn = document.getElementById('software-projects-btn');
+const aiProjects = document.getElementById('ai-projects');
+const softwareProjects = document.getElementById('software-projects');
+
 
 function setNightModeState() {
   const nightMode = localStorage.getItem('nightMode');
-  
+
   if (nightMode === 'enabled') {
     document.body.classList.add('night-mode');
     themeToggle.textContent = 'Light Mode';
@@ -12,9 +17,29 @@ function setNightModeState() {
   }
 }
 
+function toggleActiveClass(btn) {
+  aiProjectsBtn.classList.remove('active');
+  softwareProjectsBtn.classList.remove('active');
+  btn.classList.add('active');
+}
+
+function filterProjects(projectType) {
+  if (projectType === 'ai') {
+    aiProjects.style.display = 'block';
+    softwareProjects.style.display = 'none';
+    toggleActiveClass(aiProjectsBtn);
+  } else {
+    aiProjects.style.display = 'none';
+    softwareProjects.style.display = 'block';
+    toggleActiveClass(softwareProjectsBtn);
+  }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   setNightModeState();
-  
+  // filterProjects('ai'); // Uncomment this to toggle AI section at loading
+  filterProjects('software'); 
+
   const projects = document.querySelectorAll('.project');
   const observerOptions = {
     root: null,
@@ -49,4 +74,16 @@ document.addEventListener('DOMContentLoaded', () => {
       localStorage.setItem('nightMode', 'enabled');
     }
   });
+
+  aiProjectsBtn.addEventListener('click', () => {
+    filterProjects('ai');
+  });
+
+  softwareProjectsBtn.addEventListener('click', () => {
+    filterProjects('software');
+  });
+
+  // toggleActiveClass(aiProjectsBtn); // Uncomment this to toggle AI section at loading
+  toggleActiveClass(softwareProjectsBtn);
 });
+
